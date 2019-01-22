fb208ff7d478dadffc8e31755c59c1e1e8b4f6f5
thecontinium <thecontinium@outlook.com>
2019-01-22 16:08:30 +0800

    initial

---
+++ Formula/setup-zsh-env.rb
@@ -0,0 +1,14 @@
+class SetupZshEnv< Formula
+  homepage 'https://github.com/thecontinium/homebrew-setup-xdg-zsh-env/'
+  url 'https://github.com/thecontinium/homebrew-setup-xdg-zsh-env.git'
+  version '0.0.1'
+
+  skip_clean 'bin'
+
+  def install
+    prefix.install Dir["config/*.plist"]
+  end
+
+  plist_options :startup  => true
+
+end
