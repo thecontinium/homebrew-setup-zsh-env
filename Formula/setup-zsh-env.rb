3318cede23f51bb269f67b194c6a883006d63fa4
thecontinium <thecontinium@outlook.com>
2019-01-22 19:01:24 +0800

    initial

--- Formula/setup-zsh-env.rb
+++ Formula/setup-zsh-env.rb
@@ -3,27 +3,8 @@ class SetupZshEnv< Formula
   url 'https://github.com/thecontinium/homebrew-setup-zsh-env.git'
   version '0.0.3'
 
-  skip_clean 'bin'
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-        <key>Label</key>
-        <string>homebrew.mxcl.setup-zsh-env</string>
-        <key>ProgramArguments</key>
-        <array>
-            <string>sh</string>
-            <string>-c</string>
-            <string>
-                launchctl setenv ZDOTDIR $HOME/.config/zsh
-            </string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-    </dict>
-    </plist>
-    EOS
+  def install
+    prefix.install Dir["config/*.plist"]
   end
+
 end
