6cc9f07938492bc5359d7601da0be7025caa2dfd
thecontinium <thecontinium@outlook.com>
2019-01-22 19:02:46 +0800

    initial

--- Formula/setup-zsh-env.rb
+++ Formula/setup-zsh-env.rb
@@ -1,7 +1,7 @@
 class SetupZshEnv< Formula
   homepage 'https://github.com/thecontinium/homebrew-setup-zsh-env/'
   url 'https://github.com/thecontinium/homebrew-setup-zsh-env.git'
-  version '0.0.3'
+  version '0.0.4'
 
   def install
     prefix.install Dir["config/*.plist"]
